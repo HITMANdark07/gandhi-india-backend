34eb2c1cdd947c7d2f3626769eefe4a36eb140a8
Shekhar kumar <shekharkumar235838@gmail.com>
2021-12-15 17:16:30 +0530

    send mail authorization

--- routes/seller.js
+++ routes/seller.js
@@ -2,12 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 const {signUp,signin, signout,list, sendSellerEmail} = require('../controllers/seller');
+const { requireSigninAdmin, isAdmin, adminById } = require("../controllers/admin");
 const { sellerSignupValidator } = require('../validator/index');
 
 router.post("/seller/signup",sellerSignupValidator, signUp);
-router.post("/seller/send/email", sendSellerEmail);
+router.post("/seller/send/email/:adminId",requireSigninAdmin, isAdmin, sendSellerEmail);
 router.post("/seller/signin", signin);
 router.get("/seller/signout", signout);
 router.get("/seller/list",list);
+
+router.param("adminId",adminById);
 
 module.exports = router;
