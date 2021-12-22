3d5162e6c2176e1ee90e27ea291d131bcf4b4b8e
Shekhar kumar <shekharkumar235838@gmail.com>
2021-12-22 20:47:51 +0530

    update orders

--- routes/order.js
+++ routes/order.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const { create, ordersByUser, orderslist, decreaseQunatity, bulkToShipped } = require('../controllers/order');
+const { create, ordersByUser, orderslist, decreaseQunatity, bulkToShipped, update } = require('../controllers/order');
 
 const { requireSigninUser, isUser, userById} = require("../controllers/user");
 const { requireSigninAdmin, isAdmin, adminById} = require("../controllers/admin");
 const { orderValidator } = require('../validator');
 
 router.post("/order/create/:userId", orderValidator, requireSigninUser, isUser,decreaseQunatity, create);
+router.post("/order/update/:adminId", requireSigninAdmin, isAdmin, update);
 router.get("/orders/:userId", requireSigninUser, isUser, ordersByUser);
 router.get("/orders/list/:adminId", requireSigninAdmin, isAdmin, orderslist);
 router.put("/order/bulk/status/:adminId", requireSigninAdmin, isAdmin,bulkToShipped);
